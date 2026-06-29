6355c208463b1d1407ee806e42b660a76067ef91
fatihua <fatihua@gmail.com>
2026-06-29 21:43:04 +0700

    feat: migrate badge registry component

---
+++ registry/examples/badge-with-icon.tsx
@@ -0,0 +1,12 @@
+import { ArrowRight } from "lucide-react"
+
+import { Badge } from "@/registry/ui/badge"
+
+export default function BadgeWithIcon() {
+  return (
+    <Badge>
+      Badge
+      <ArrowRight />
+    </Badge>
+  )
+}
