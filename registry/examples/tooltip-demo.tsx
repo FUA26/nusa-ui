b62ff68fc9c4627f5962790916d959989498655d
fatihua <fatihua@gmail.com>
2026-06-30 08:10:55 +0700

    feat: migrate separator tooltip collapsible and progress components

---
+++ registry/examples/tooltip-demo.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/registry/ui/button"
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/registry/ui/tooltip"
+
+export default function TooltipDemo() {
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button variant="outline">Hover</Button>
+      </TooltipTrigger>
+      <TooltipContent>Tooltip text</TooltipContent>
+    </Tooltip>
+  )
+}
